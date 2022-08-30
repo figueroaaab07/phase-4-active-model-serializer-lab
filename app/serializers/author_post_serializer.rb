c23aa6feea8f143b7ae82d18a15e4cb71481ef65
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-30 16:15:57 -0700

    Completed Assignment

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -2,7 +2,6 @@ class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content
 
   belongs_to :author
-  has_many :post_tags
   has_many :tags, through: :post_tags
 
   def short_content
